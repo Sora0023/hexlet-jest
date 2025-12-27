127b482ed70b4d3343f34fbdcf7ebfa1108c651f
Sora0023 <207861656+Sora0023@users.noreply.github.com>
2025-12-27 17:03:00 +1000

    Update index(.test).js fixtures are used for jest

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,6 +1,27 @@
+/* eslint-disable */
+
+import fs from 'fs'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { beforeEach, expect, test } from '@jest/globals'
+
 import reverse from '../src/index.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
+const readFile = filename => fs.readFileSync(getFixturePath(filename), 'utf-8')
 
 test('reverse', () => {
   expect(reverse('hello')).toEqual('olleh')
   expect(reverse('')).toEqual('')
+});
+
+test('reverse', () => {
+  const text = readFile('lore.txt').trim()
+  const result = readFile('lore_reverse.txt')
+
+  expect(reverse(text)).toEqual(result)
 })
+
